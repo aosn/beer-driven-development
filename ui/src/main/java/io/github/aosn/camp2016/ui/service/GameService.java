b9f28729f86ae77894354be716d7cda839718a8c
Akihiro Kimura <aki.kimura@opt.ne.jp>
2016-09-18 02:20:18 +0900

    feat: Add GameService

---
+++ ui/src/main/java/io/github/aosn/camp2016/ui/service/GameService.java
@@ -0,0 +1,12 @@
+package io.github.aosn.camp2016.ui.service;
+
+public interface GameService {
+
+    /**
+     * Create new game
+     *
+     * @param userNames The names of users separated by `\n`
+     * @return succeeded | failed
+     */
+    boolean create(String userNames);
+}
