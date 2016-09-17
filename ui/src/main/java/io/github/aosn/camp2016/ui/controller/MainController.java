ba8e0fbd6067198bc5b5a1896c3b8a45e8d59cc5
mikan <yutakakato@icloud.com>
2016-09-18 01:30:18 +0900

    Complete main.fxml

--- ui/src/main/java/io/github/aosn/camp2016/ui/controller/MainController.java
+++ ui/src/main/java/io/github/aosn/camp2016/ui/controller/MainController.java
@@ -2,10 +2,13 @@ package io.github.aosn.camp2016.ui.controller;
 
 import io.github.aosn.camp2016.ui.Launcher;
 import io.github.aosn.camp2016.ui.util.Logs;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -24,19 +27,100 @@ public class MainController implements Initializable {
     @FXML
     public VBox wrapper;
     @FXML
-    private Label statusLabel;
+    public Label dice1;
+    @FXML
+    public Label dice2;
+    @FXML
+    public Button shuffleButton;
+    @FXML
+    public HBox userList;
+    @FXML
+    public VBox cell01;
+    @FXML
+    public VBox cell02;
+    @FXML
+    public VBox cell03;
+    @FXML
+    public VBox cell04;
+    @FXML
+    public VBox cell05;
+    @FXML
+    public VBox cell06;
+    @FXML
+    public VBox cell07;
+    @FXML
+    public VBox cell08;
+    @FXML
+    public VBox cell09;
+    @FXML
+    public VBox cell10;
+    @FXML
+    public VBox cell11;
+    @FXML
+    public VBox cell12;
+    @FXML
+    public VBox cell13;
+    @FXML
+    public VBox cell14;
+    @FXML
+    public VBox cell15;
+    @FXML
+    public VBox cell16;
+    @FXML
+    public VBox cell17;
+    @FXML
+    public VBox cell18;
+    @FXML
+    public VBox cell19;
+    @FXML
+    public VBox cell20;
+    @FXML
+    public VBox cell21;
+    @FXML
+    public VBox cell22;
+    @FXML
+    public VBox cell23;
+    @FXML
+    public VBox cell24;
+    @FXML
+    public VBox cell25;
+    @FXML
+    public VBox cell26;
+    @FXML
+    public VBox cell27;
+    @FXML
+    public VBox cell28;
+    @FXML
+    public VBox cell29;
+    @FXML
+    public VBox cell30;
+    @FXML
+    public VBox cell31;
+    @FXML
+    public VBox cell32;
+    @FXML
+    public VBox cell33;
+    @FXML
+    public VBox cell34;
+    @FXML
+    public VBox cell35;
+    @FXML
+    public VBox cell36;
+    @FXML
+    public VBox cell37;
+    @FXML
+    public VBox cell38;
+    @FXML
+    public VBox cell39;
+    @FXML
+    public VBox cell40;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Logs.applyLogLevel(log);
 
-        // Setup status label.
-        try {
-            statusLabel.setText(InetAddress.getLocalHost().toString());
-        } catch (UnknownHostException e) {
-            log.throwing(MainController.class.getName(), "initialize", e);
-            statusLabel.setText("[ERROR] " + e.getMessage());
-        }
+        dice1.setText("0");
+        dice2.setText("0");
 
         log.info(MainController.class.getSimpleName() + " initialized.");
     }
@@ -63,24 +147,6 @@ public class MainController implements Initializable {
         }
     }
 
-    /**
-     * Click handler for "clock" button.
-     *
-     * @param event mouse event
-     */
-    @FXML
-    public void onClockClicked(MouseEvent event) {
-        log.info("Clock button clicked.");
-        Launcher.setScreen(getClass(), "clock", Launcher.getPrimaryStage().isFullScreen());
-    }
-
-    /**
-     * Click handler for "countdown" button.
-     *
-     * @param event mouse event
-     */
-    @FXML
-    public void onTimerClicked(MouseEvent event) {
-        Launcher.setScreen(getClass(), "timer", Launcher.getPrimaryStage().isFullScreen());
+    public void inShuffleClicked(Event event) {
     }
 }
