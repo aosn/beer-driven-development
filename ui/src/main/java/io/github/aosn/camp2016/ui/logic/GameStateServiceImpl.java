ecb1112b0525585a5f34e555f26db31cd7cbe9a7
Akihiro Kimura <aki.kimura@opt.ne.jp>
2016-09-18 11:51:43 +0900

    feat: Add mocked backend

--- ui/src/main/java/io/github/aosn/camp2016/ui/logic/GameStateServiceImpl.java
+++ ui/src/main/java/io/github/aosn/camp2016/ui/logic/GameStateServiceImpl.java
@@ -2,15 +2,20 @@ package io.github.aosn.camp2016.ui.logic;
 
 import io.github.aosn.camp2016.ui.entity.GameState;
 import io.github.aosn.camp2016.ui.service.GameStateService;
-import io.github.aosn.camp2016.ui.util.HttpClient;
+import io.github.aosn.camp2016.ui.service.HttpClient;
 
 public class GameStateServiceImpl implements GameStateService {
 
+    private final HttpClient httpClient;
+
+    public GameStateServiceImpl(HttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
     @Override
     public GameState get() {
-        return JsonDeserializer.deserialize(
-                //FIXME Don't use mock
-                HttpClient.getMock("/bdd/game/0/state").get(),
+        return JsonSerializer.deserialize(
+                httpClient.get("/bdd/game/0/state").get(),
                 GameState.class
         )
                 .orElseThrow(() -> new RuntimeException("GameState is not found."));
@@ -18,8 +23,7 @@ public class GameStateServiceImpl implements GameStateService {
 
     @Override
     public boolean update(GameState gameState) {
-        //FIXME Don't use mock
-        return HttpClient.putMock(
+        return httpClient.put(
                 "/bdd/game/0/change",
                 gameState);
     }
