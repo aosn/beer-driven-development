d58ca6bf0b12346ed04830c98b7a2134acc5f170
LagunaPresa <sunspotactivity@gmail.com>
2016-09-22 17:50:30 +0900

    wip feat(be-ps): Add Game.create API

--- backend-play-scala/app/controllers/GameController.scala
+++ backend-play-scala/app/controllers/GameController.scala
@@ -2,12 +2,16 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import play.api.mvc.Controller
+import play.api.mvc.{Action, Controller}
 import services.GameService
 
 @Singleton
 class GameController @Inject()(service: GameService) extends Controller with BaseController {
-  def create() = ???
+
+  def create() = Action { implicit req =>
+    Ok("")
+  }
+
   def show(gameId: Int) = ???
   def update(gameId: Int) = ???
 }
